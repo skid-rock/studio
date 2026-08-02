223370a94af4f7cc5891cb46efabdcbea96f6bc1
Alexey Yamberg <yambergaa@gmail.com>
2026-08-02 17:59:34 +0700

    STUDIO-044: экспорт дизайн-системы из Designer
    Токены, слой ch-*, витрины и шаблоны; старые editor/references удалены.

---
+++ docs/design/_ds_bundle.js
@@ -0,0 +1,11 @@
+/* @ds-bundle: {"format":4,"namespace":"Studio_cb6ef0","components":[],"sourceHashes":{},"inlinedExternals":[],"unexposedExports":[]} */
+
+(() => {
+
+const __ds_ns = (window.Studio_cb6ef0 = window.Studio_cb6ef0 || {});
+
+const __ds_scope = {};
+
+(__ds_ns.__errors = __ds_ns.__errors || []);
+
+})();
